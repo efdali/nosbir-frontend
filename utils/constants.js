2cef3e07bef86e973b99b117c00922e4c802b8f6
efdali <efdal_incesu@hotmail.com>
2019-12-10 22:58:14 +0300

    image icon change

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
 export const API_URL = 'http://kuzenbilisim.com/api/v0/';
 export const IMAGE_URL= 'http://kuzenbilisim.com/api/resimler/';
-export const GROUP_IMAGE_URL= 'http://kuzenbilisim/api/gruplar/';
+export const GROUP_IMAGE_URL= 'http://kuzenbilisim.com/api/gruplar/';
 //export const API_URL = 'http://localhost/nosbir-backend/v0/';
